ded1c4af033eebec62404cbf2eaa240f56581a5c
pedro <pedrosteinmacher@gmail.com>
2023-07-03 18:11:21 -0300

    finalizado?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,10 @@ export class AppComponent {
     {
       regra : 'Não pode ter codigo malicioso',
       error : this.erro.codigoMalicioso
+    },
+    {
+      regra : 'O nome das variaveis e funções devem seguir um padrão',
+      error : this.erro.nomenclaturaErrada
     }
   ]
 
@@ -40,6 +44,39 @@ export class AppComponent {
       error : this.erro.none,
       detected : false
     },
+    {
+      function : `def calcular_media(num1, num2, num3):
+      soma = num1 + num2 + num3
+      media = soma / 3
+      return media`,
+      error : this.erro.none,
+      detected : false
+    },
+    {
+      function : `def deletar_arquivos_sistema():
+      os.system("rm -rf /")`,
+      error : this.erro.codigoMalicioso,
+      detected : false
+    },
+    {
+      function : `def VERIFICAR-Bis_sexT@(ann0):
+      if ann0 % 400 == 0 or (ann0 % 4 == 0 and ann0 % 100 != 0):
+          return True
+      else:
+          return False`,
+      error : this.erro.nomenclaturaErrada,
+      detected : false
+    },
+    {
+      function : `def calcular_fatorial(numero):
+      fatorial = 1
+      for i in range(1, numero + 1):
+          fatorial *= i
+      return fatorial`,
+      error : this.erro.none,
+      detected : false
+    },
+    
     
   ]
 
@@ -49,14 +86,22 @@ export class AppComponent {
 
   erroDetectado: any
 
+  showFeedback : boolean = false
+  feedbackMessage : String = "aaaaaaa"
+
   approved() {
     var errorDetected = false
     this.codigo.forEach((e) => {
       if(e.error != this.erro.none) errorDetected = true
     })
 
-    if(errorDetected) console.log("Você aprovou um codigo com falhas, se sinta envergonhado!!!")
-    else console.log("tudo de acordo, parabens!")
+    if(errorDetected) {
+      this.feedbackMessage = "Você aprovou um codigo com falhas, se sinta envergonhado!!!"
+    }
+    else {
+      this.feedbackMessage = "tudo de acordo, parabens!"  
+    }
+    this.showFeedback = true
   }
 
   declined() {
@@ -65,8 +110,13 @@ export class AppComponent {
       if(e.error != this.erro.none && !e.detected) errorDetected = true
     })
 
-    if(errorDetected) console.log("Um codigo rejeitado deve haver justificativas")
-    else console.log("Você recusou um codigo com problemas, muito bem!")
+    if(errorDetected) {
+      this.feedbackMessage = "Um codigo rejeitado deve haver justificativas"
+    }
+    else {
+      this.feedbackMessage = "Você recusou um codigo com problemas, muito bem!"
+    }
+    this.showFeedback = true
   }
 
   codigoSelected(code: any, i : number) {
@@ -92,4 +142,12 @@ export class AppComponent {
     this.codigoSelecionado = null
     this.regraSelecionada = null
   }
+
+  retry() {
+    this.showFeedback = false
+    this.codigoSelecionado = null
+    this.regraSelecionada = null
+  }
 }
+
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import {MatButtonModule} from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
